4da088c35c345369cf089710f0a8636b3a1585c1
Tommy Bojanin <bojanint@gmail.com>
2025-06-06 20:38:07 -0700

    upgrade visibility of overrides, perhaps not needed

--- librt/tsan_ipc.cc
+++ librt/tsan_ipc.cc
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 #include "sanitizer/tsan_interface.h"  // optional but safe
-
-extern "C" void _tsan_on_report(void* report) {
+__attribute__((visibility("default"))) extern "C" void __tsan_on_report(
+    void* report) {
   printf("\n\n\n\n\n\n\n\nasdf\n\n\n\n\n\n");
   fprintf(stderr, "[rtlib] >>> __tsan_on_report() intercepted!\n");
 }
-extern "C" void _sanitizer_report_error_summary(const char* summary) {
+__attribute__((visibility("default"))) extern "C" void
+__sanitizer_report_error_summary(const char* summary) {
   /* forward to GUI / IPC – for now just log */
   fprintf(stderr, "[rtlib] SUMMARY HOOK: %s\n", summary);
+}
+__attribute__((visibility("default"))) extern "C" void __sanitizer_on_print(
+    const char* msg, size_t len) {
+  /* very noisy: filter only ThreadSanitizer lines */
+  fwrite(msg, 1, len, stderr); /* or send over IPC */
+
+  /* example: detect end of report */
+  fprintf(stderr, "[rtlib] --- end of TSan report ---\n");
 }
