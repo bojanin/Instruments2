670be9f96e8a09612add5de98b9b4f294a98ba65
Tommy Bojanin <bojanint@gmail.com>
2025-06-06 20:21:12 -0700

    upgraded command line tools and homebrew packages

--- librt/tsan_ipc.cc
+++ librt/tsan_ipc.cc
@@ -6,3 +6,7 @@ extern "C" void _tsan_on_report(void* report) {
   printf("\n\n\n\n\n\n\n\nasdf\n\n\n\n\n\n");
   fprintf(stderr, "[rtlib] >>> __tsan_on_report() intercepted!\n");
 }
+extern "C" void _sanitizer_report_error_summary(const char* summary) {
+  /* forward to GUI / IPC – for now just log */
+  fprintf(stderr, "[rtlib] SUMMARY HOOK: %s\n", summary);
+}
