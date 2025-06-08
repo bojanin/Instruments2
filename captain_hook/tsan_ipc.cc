8cc3130d431c496fa46a431bd81c675865dac8dc
Tommy Bojanin <bojanint@gmail.com>
2025-06-07 19:44:28 -0700

    Write some sample code that generates a TSAN report.

--- captain_hook/tsan_ipc.cc
+++ captain_hook/tsan_ipc.cc
@@ -2,7 +2,7 @@
 
 #include <print>
 
-#include "sanitizer/tsan_interface.h"  // optional but safe
+#include "sanitizer/tsan_interface.h"
 __attribute__((constructor)) void Init() {
   //
 }
@@ -12,7 +12,34 @@ __attribute__((destructor)) void Deinit() {
 }
 
 extern "C" void __tsan_on_report(void* report) {
-  __tsan_get_current_report();
-  //
-  std::print("Here\n");
+  const char* description;
+  int count;
+  int stack_count, mop_count, loc_count, mutex_count, thread_count,
+      unique_tid_count;
+  void* sleep_trace[16] = {0};
+  __tsan_get_report_data(report, &description, &count, &stack_count, &mop_count,
+                         &loc_count, &mutex_count, &thread_count,
+                         &unique_tid_count, sleep_trace, 16);
+  fprintf(stderr, "report type = '%s', count = %d\n", description, count);
+  fprintf(stderr, "mop_count = %d\n", mop_count);
+  int tid;
+  void* addr;
+  int size, write, atomic;
+  void* trace[16] = {0};
+
+  __tsan_get_report_mop(report, 0, &tid, &addr, &size, &write, &atomic, trace,
+                        16);
+  uint64_t os_id;
+  int running;
+  const char* name;
+  int parent_tid;
+
+  __tsan_get_report_thread(report, 0, &tid, &os_id, &running, &name,
+                           &parent_tid, trace, 16);
+  fprintf(stderr, "tid = %d\n", tid);
+  // CHECK: tid = 1
+
+  __tsan_get_report_thread(report, 1, &tid, &os_id, &running, &name,
+                           &parent_tid, trace, 16);
+  fprintf(stderr, "tid = %d\n", tid);
 }
